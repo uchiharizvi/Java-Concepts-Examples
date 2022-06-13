2a8e051df06532a3b792d03af8953af56542e44b
Kavish Rizvi <rizvikavish205.com>
2022-06-14 00:20:09 +0530

    Singleton example

---
+++ src/main/java/org/java/tutorials/designpatterns/creational/Singleton.java
@@ -0,0 +1,20 @@
+package org.java.tutorials.designpatterns.creational;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+
+        if (instance == null)
+            instance = new Singleton();
+
+        return instance;
+    }
+
+
+    public void doSomething() {
+    }
+}
--- src/main/java/org/java/tutorials/designpatterns/creational/SingletonExample.java
+++
@@ -1,7 +0,0 @@
-package org.java.tutorials.designpatterns.creational;
-
-public class SingletonExample {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
