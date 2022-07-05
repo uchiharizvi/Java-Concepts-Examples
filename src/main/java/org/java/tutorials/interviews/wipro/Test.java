233b43279bf02dad58a641641e988ecea6bb2112
Kavish Rizvi <rizvikavish205@gmail.com>
2022-07-05 12:46:58 +0530

    Wipro Test

---
+++ src/main/java/org/java/tutorials/interviews/wipro/Test.java
@@ -0,0 +1,20 @@
+package org.java.tutorials.interviews.wipro;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "I am living in Bangalore";
+        char[] arr = s1.toCharArray();
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        for(char elem:arr) {
+            if (!frequency.containsKey(elem)) {
+                frequency.put(elem, 1);
+
+            } else {
+                frequency.put(elem, frequency.get(elem) + 1);
+            }
+        }
+        System.out.println(frequency);
+    }
+}
